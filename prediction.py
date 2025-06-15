955d3b079492907c6831afa35d702d20696c57cb
Kali Live user <bangiqballah@gmail.com>
2025-06-15 22:59:19 +0700

    fix: predict

--- prediction.py
+++ prediction.py
@@ -45,17 +45,6 @@ def data_preprocessing(input_array):
     df_scaled = pd.DataFrame(scaled, columns=FEATURE_ORDER)
     return df_scaled
 
-def prediction(processed_data, input_array):
-    # --- RULE BASED OVERRIDE ---
-    tuition_fees = input_array[11]  # indeks ke-11 dari input_array (Tuition_fees_up_to_date)
-    
-    # Ambil nilai dari input_array
-    grade_avg = (float(input_array[4]) + float(input_array[5])) / 2  # rata-rata grade
-    unit_approved_total = int(input_array[2]) + int(input_array[3])  # total unit approved
-
-    if tuition_fees == "Ya" and grade_avg >= 10 and unit_approved_total >= 7:
-        return "Graduate"
-
-    # --- ML BASED PREDICTION ---
+def prediction(processed_data):
     pred = model.predict(processed_data)[0]
-    return "Graduate" if pred == 0 else "Dropout"
+    return "Graduate" if pred >= 0 else "Dropout"
